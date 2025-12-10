ea5857b9e674e15afe91ee264e41356461e73e5a
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-10 17:07:23 +0200

    edit compass

--- src/pages/qiblaCompass.jsx
+++ src/pages/qiblaCompass.jsx
@@ -6,33 +6,29 @@ import compassArrow from '../assets/qiblaArrow.png';
 
 const QiblaCompass = () => {
 
-  const [direction, setDirection] = useState(0);   // اتجاه القبلة
-  const [deviceDeg, setDeviceDeg] = useState(0);   // اتجاه الموبايل (البوصلة)
-
+  const [direction, setDirection] = useState(0);
+  // const [deviceDeg, setDeviceDeg] = useState(0);
   useEffect(() => {
-    // 1) GPS
     navigator.geolocation.getCurrentPosition(
       async (position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
 
         try {
-          // 2) Data from API
-          const res = await axios.get(`https://api.aladhan.com/v1/qibla/${lat}/${lon}`);
-          setDirection(res.data.data.direction);
+          const resonse = await axios.get(`https://api.aladhan.com/v1/qibla/${lat}/${lon}`);
+          setDirection(resonse.data.data.direction);
         } catch (error) {
           toast.error("Error fetching Qibla direction");
         }
-
       },
       () => toast.error("قم بتفعيل الـ GPS")
     );
 
-    // 3) اتجاه الجهاز (البوصلة)
-    window.addEventListener("deviceorientation", (event) => {
-      const alpha = event.alpha;  // اتجاه الجهاز
-      setDeviceDeg(alpha);
-    });
+    // // 3) اتجاه الجهاز (البوصلة)
+    // window.addEventListener("deviceorientation", (event) => {
+    //   const alpha = event.alpha;  // اتجاه الجهاز
+    //   setDeviceDeg(alpha);
+    // });
 
   }, []);
 
@@ -40,27 +36,11 @@ const QiblaCompass = () => {
     <section className='qibla-compass'>
       <div className='container qibla-container'>
         <h2>اتجاه القبلة</h2>
-
         <div className='img-container'>
-
-          {/* البوصلة تتحرك مع الجهاز */}
-          <img 
-            className='compass-img'
-            src={compassImg}
-            alt="compass"
-            style={{ transform: `rotate(${-deviceDeg}deg)` }}
-          />
-
-          {/* السهم يتوجه للقبلة */}
-          <img
-            className='arrow-img'
-            src={compassArrow}
-            alt="arrow"
-            style={{ transform: `rotate(${direction - deviceDeg}deg) scale(0.4)` }}
-          />
-
+          {/* <img className='compass-img' src={compassImg} alt="compass" style={{ transform: `rotate(${-deviceDeg}deg)` }} /> */}
+          <img  className='compass-img' src={compassImg} alt="compass" />
+          <img className='arrow-img' src={compassArrow} alt="arrow" style={{ transform: `rotate(${direction}deg) scale(0.4)`}}/>
         </div>
-
         <p>زاوية القبلة: {direction.toFixed(2)}°</p>
 
       </div>
